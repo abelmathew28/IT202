4ceb2109dbbb3ea2cc2a6f5dd8dbf0af9913d007
abelmathew28@gmail.com <ab2323@afsconnect1.njit.edu>
2019-09-17 22:53:02 -0400

    testing .gitignore for config.php

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+host="sql1.njit.edu";//your msql host [sql1, sql2, sql3]
+$database="ab2323";//ucid
+$username="ab2323";//ucid
+$password="Bethel2012$";//mysql password (not ucid password)
+?>
