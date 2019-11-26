2873a7ea932499941c78d74835c7261db7b0d8c3
abel <ab2323@afsconnect2.njit.edu>
2019-11-25 22:25:30 -0500

    registration action

---
+++ BankingApp/registerAction.php
@@ -0,0 +1,58 @@
+<?php 
+
+require_once 'db_connect.php';
+
+if($_POST) {
+	$fname = $_POST['fname'];
+	$lname = $_POST['lname'];
+	$Address1 = $_POST['Address1'];
+	$state = $_POST['state'];
+	$zipcode = $_POST['zipcode'];
+	$account_type = $_POST['account_type'];
+	$security_question = $_POST['security_questions'];
+	$answer = $_POST['answer'];
+	$phone = $_POST['phone'];
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	$user ="user";
+	$status ="active";
+	$date =date("Y/m/d");
+	$pswdencrypted = password_hash($password, PASSWORD_DEFAULT);
+	$sql = "INSERT INTO login (username, passcode, usertype, status) VALUES ('$username', '$pswdencrypted', '$user', '$status')";
+
+	if($connect->query($sql) === TRUE) {
+	 $login_id = $connect->insert_id;
+		$account_number = "select * from holders_detail order by `detail_id` desc limit 1";
+		$accresult = $connect->query($account_number);
+		$data = $accresult->fetch_assoc();
+
+			if(empty($data)) {
+				$bank_account_number="900000000101";
+			} else {
+					$curaccno = $data['account_number'];
+				$bank_account_number= $curaccno + 1;
+			}
+
+			$account_holders = "INSERT INTO account_holders (first_name, Last_name, address1,address2,state,zip_code,phone,email,login_id, status) VALUES ('$fname', '$lname', '$Address1','$Address2', '$state', '$zipcode','$phone', '$username', '$login_id', '$status')";
+
+			if($connect->query($account_holders) === TRUE) {
+				$account_holderid = $connect->insert_id;
+				$account_holders_details = "INSERT INTO holders_detail (holder_id,account_number, account_type,security_question_id,answer,initial_deposit,current_balance,last_transaction_date, last_transaction_amount) VALUES ('$account_holderid','$bank_account_number', '$account_type','$security_question', '$answer', 0,0, '$date', 0)";
+				$connect->query($account_holders_details);
+			}
+			else{
+				echo "Error " . $sql . ' ' . $connect->connect_error;
+			}
+
+
+		echo "<h3>Hi  $fname  Your  Successfully Created your Account</h3>";
+		echo "<p>Your Account Number is : $bank_account_number</p>";
+		echo "<a href='index.php'>Click here to  Login </a>";	
+	} else {
+		echo "Error " . $sql . ' ' . $connect->connect_error;
+	}
+
+	$connect->close();
+}
+
+?>
