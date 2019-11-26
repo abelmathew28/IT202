05d216aabe798c0134dc860ad67b0914799e9d93
abel <ab2323@afsconnect2.njit.edu>
2019-11-25 22:06:17 -0500

    ADDING account withdraw

---
+++ BankingApp/account_withdraw_action.php
@@ -0,0 +1,33 @@
+<?php 
+
+require_once 'db_connect.php';
+
+if($_POST) {
+
+	$amount = $_POST['amount'];
+	$balance = $_POST['balance'];
+	$detailsid = $_POST['detailsid'];
+	$account_type = $_POST['account_type'];
+	$account_number = $_POST['account_number'];
+	$status ="active";
+	$date =date("Y/m/d");
+	
+	$account_activity = "INSERT INTO account_activity (transaction_type, amount,effective_balance, depositer_detail_id,recipient_detail_id,activity_date,status) VALUES ('withdrawal', '$amount','$balance','$detailsid','1', '$date', '$status')";
+	if($connect->query($account_activity)){
+
+		$withdrawal_detail_update  = "UPDATE holders_detail SET current_balance = '$balance', last_transaction_date = '$date', last_transaction_amount = '$amount', transaction_type = 'withdrawal' WHERE detail_id = {$detailsid}";
+		$connect->query($withdrawal_detail_update) ;
+
+		echo "<h3>Hi Successfully withdraw Amount From $account_number </h3>";
+
+		echo "<p>Your Current Balance is : $ $balance</p>";
+			echo "<a href='home.php'>Click here to  Home </a>";	
+	} else {
+			echo "<h3>Hi Invalid Transaction please try again later</h3>";
+			echo "<a href='home.php'>Click here to  Home  </a>";
+	}
+
+	$connect->close();
+}
+
+?>
