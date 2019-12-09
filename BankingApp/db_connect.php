e8924ae323e92756e1f51da18ab211ddf0fee293
abel <ab2323@afsconnect2.njit.edu>
2019-12-09 09:55:37 -0500

    updating db_connect.php

--- BankingApp/db_connect.php
+++ BankingApp/db_connect.php
@@ -1,10 +1,11 @@
 <?php 
 
-$localhost = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "Banking_app";
+$localhost="sql1.njit.edu";//your msql host [sql1, sql2, sql3]
+$dbname="ab2323";//ucid
+$username="ab2323";//ucid
+$password="r0IgvuRjH";//mysql password (not ucid password)
 
+echo var_export($password);
 // create connection
 $connect = new mysqli($localhost, $username, $password, $dbname);
 
