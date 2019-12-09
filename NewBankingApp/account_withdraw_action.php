4a3c2d878bddd9997f7d901ed5ec06147fa40659
abel <ab2323@afsconnect2.njit.edu>
2019-12-09 18:43:27 -0500

    account withdraw

---
+++ NewBankingApp/account_withdraw_action.php
@@ -0,0 +1,34 @@
+<?php 
+
+require_once 'db_connect.php';
+
+if($_POST) {
+
+	$amount = $_POST['amount'];
+	$balance = $_POST['balance'];
+	$detailsid = $_POST['detailsid'];
+	$account_type = $_POST['account_type'];
+	$account_number = $_POST['account_number'];
+	$status ="active";
+	$date =date("Y/m/d");
+	
+	$account_activity = "INSERT INTO account_activity (transaction_type, amount,effective_balance, depositer_detail_id,recipient_detail_id,transaction_show,activity_date,status) VALUES ('withdrawal', '-$amount','$balance','$detailsid','1',1, '$date', '$status')";
+	if($connect->query($account_activity)){
+		$account_activity = "INSERT INTO account_activity (transaction_type, amount,effective_balance, depositer_detail_id,recipient_detail_id,transaction_show,activity_date,status) VALUES ('withdrawal', '$amount','$amount','1','$detailsid',2, '$date', '$status')";
+		$connect->query($account_activity);
+		$withdrawal_detail_update  = "UPDATE holders_detail SET current_balance = '$balance', last_transaction_date = '$date', last_transaction_amount = '$amount', transaction_type = 'withdrawal' WHERE detail_id = {$detailsid}";
+		$connect->query($withdrawal_detail_update) ;
+
+		echo "<h3>Hi Successfully withdraw Amount From $account_number </h3>";
+
+		echo "<p>Your Current Balance is : $ $balance</p>";
+			echo "<a href='accountActivity.php'>Click here to  Home </a>";	
+	} else {
+			echo "<h3>Hi Invalid Transaction please try again later</h3>";
+			echo "<a href='accountActivity.php'>Click here to  Home  </a>";
+	}
+
+	$connect->close();
+}
+
+?>
