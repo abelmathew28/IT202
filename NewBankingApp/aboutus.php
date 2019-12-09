2e16018b2ef03091c3b2984b3d07caff7928edae
abel <ab2323@afsconnect2.njit.edu>
2019-12-09 18:45:37 -0500

    about us

---
+++ NewBankingApp/aboutus.php
@@ -0,0 +1,39 @@
+
+<html>
+<title>NJIT Aboutus Page</title>
+<link href="style.css" rel="stylesheet" type="text/css"></link>
+
+   </head>
+   
+   <body >
+    <div class="fixed-header">
+      <div class="containers">
+        <nav>
+            <a href="index.php">Home</a>
+           <a href="userregister.php">Register</a>
+          <a href="aboutus.php">About Bank</a>
+          <a href="product.php">Product</a>
+		      <a href="contactus.php">Contact Us</a>
+          
+        </nav>
+      </div>
+    </div>
+	<div class="contactus">
+         <div class="main-text">
+            <h2><b>ABOUT US</b></h2>
+            <h3>Get the most out of your NJIT Bank accounts</h3>
+            <p>Find info on banking services, read articles, watch videos, learn how to manage your money and more.
+            </p>
+          <h3>Tax resource center</h3>
+          <p>Find the important dates and tools you need for tax time.</p>
+          <img src = "images/1.jpg" alt = "HTML Map" border = "0" usemap = "#html"/>
+           <h3>Understanding your credit score and report</h3>
+          <p>Learn about your credit score and what steps you can take to improve it.</p>
+          <img src = "images/3.jpg" alt = "HTML Map" border = "0" usemap = "#html"/>
+           <h3>International travel tips</h3>
+          <p>Prepare for travel with tips on what do to before you leave, plus info on using cash and credit cards.</p>
+          <img src = "images/2.jpg" alt = "HTML Map" border = "0" usemap = "#html"/>
+         </div>
+      </div>
+   </body>
+</html>
