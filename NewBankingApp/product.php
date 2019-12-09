f2f7b5bcabea1806901579eaeeb480cd9bfcac9b
abel <ab2323@afsconnect2.njit.edu>
2019-12-09 18:26:16 -0500

    product

---
+++ NewBankingApp/product.php
@@ -0,0 +1,66 @@
+
+<html>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<title>NJIT Product Page</title>
+<link href="style.css" rel="stylesheet" type="text/css"></link>
+<style>
+#banner {
+  display: block;
+}
+
+.images {
+  display: inline-block;
+  max-width: 20%;
+  margin: 0 2.5%;
+  padding:5px;
+}
+.details{
+  margin:0 2.5%;
+  padding:5px;
+}
+
+</style>
+   </head>
+   
+   <body >
+    <div class="fixed-header">
+      <div class="containers">
+        <nav>
+          <a href="index.php">Home</a>
+           <a href="userregister.php">Register</a>
+          <a href="aboutus.php">About Bank</a>
+          <a href="product.php">Product</a>
+		      <a href="contactus.php">Contact Us</a>
+          
+        </nav>
+      </div>
+    </div>
+	<div class="contactus">
+         <div class="product-text">
+           <div class="details">
+            <h2><b>Products</b></h2>
+            <p> NJIT Bank provides the following products. For more information contact us at 1-888-NJIT-MLK</p>
+            </div>
+            <div id="banner">
+        <div class="images">
+            <img src = "images/cheking.png">
+        </div>
+
+        <div class="images">
+            <img src = "images/savings.png">
+        </div>
+         <div class="images">
+            <img src = "images/creditcard.png">
+        </div>
+         <div class="images">
+            <img src = "images/home.png">
+        </div>
+
+        <div class="images">
+            <img src = "images/loan.png">
+        </div>
+    </div>
+      </div>
+   </body>
+</html>
+
