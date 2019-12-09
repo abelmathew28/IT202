8a23acf72a3c05428d30151f6ab04d3e6afc4360
abel <ab2323@afsconnect2.njit.edu>
2019-12-09 18:35:21 -0500

    db_connect

---
+++ NewBankingApp/db_connect.php
@@ -0,0 +1,19 @@
+<?php 
+
+$localhost = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "Banking_app";
+
+// create connection
+$connect = new mysqli($localhost, $username, $password, $dbname);
+
+// check connection
+if($connect->connect_error) {
+	die("connection failed : " . $connect->connect_error);
+} else {
+	// echo "Successfully Connected";
+}
+
+?>
+
