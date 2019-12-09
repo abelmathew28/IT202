f18da6d76b7e66ae816fe1cbe215d0dac0fe3f08
abel <ab2323@afsconnect2.njit.edu>
2019-12-09 18:36:53 -0500

    contactus

---
+++ NewBankingApp/contactus.php
@@ -0,0 +1,35 @@
+
+<html>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<title>NJIT Contact Page</title>
+<link href="style.css" rel="stylesheet" type="text/css"></link>
+
+   </head>
+   
+   <body >
+    <div class="fixed-header">
+      <div class="containers">
+        <nav>
+            <a href="index.php">Home</a>
+           <a href="userregister.php">Register</a>
+          <a href="aboutus.php">About Bank</a>
+          <a href="product.php">Product</a>
+		      <a href="contactus.php">Contact Us</a>
+          
+        </nav>
+      </div>
+    </div>
+	<div class="contactus">
+         <div class="main-text">
+            <h2><b>Bank of NJIT</b></h2>
+            <h3>323 Dr M.L.K. Jr. Blvd<br/>
+City: Newark<br/>
+State: New Jersey<br/>
+Zip: 07102<br/>
+Phone: 1-888-NJIT-MLK
+</h3>
+
+         </div>
+      </div>
+   </body>
+</html>
