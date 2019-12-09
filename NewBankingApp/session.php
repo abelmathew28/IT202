010dc44b6c5c09693d23bed50ea897986b49928e
abel <ab2323@afsconnect2.njit.edu>
2019-12-09 18:24:34 -0500

    session

---
+++ NewBankingApp/session.php
@@ -0,0 +1,29 @@
+<?php
+  require_once 'db_connect.php';
+   session_start();
+   
+   $user_name= $_SESSION['userlogin'];
+   
+   $sql = "SELECT account_holders.`holder_id` ,account_holders.`first_name`, holders_detail.`detail_id` FROM account_holders ,holders_detail WHERE account_holders.`holder_id` = holders_detail.`holder_id` and account_holders.email = '$username'";
+   $result = $connect->query($sql);
+
+   if($result->num_rows > 0) {
+
+ 	   $result_data = $result->fetch_assoc();
+      $_SESSION['userId'] = $result_data['holder_id'];
+      $_SESSION['user_detailId'] = $result_data['detail_id']; 
+      $_SESSION['user_name'] = $result_data['first_name']; 
+       if($_SESSION['usertype']=="admin"){
+            header("location: adminhome.php");
+       }
+       else{
+            header("location: home.php");
+       }
+   
+
+      }else {
+          header("location:index.php");
+   }
+   
+?>
+
